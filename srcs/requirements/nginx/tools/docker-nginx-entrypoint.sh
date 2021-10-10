13a5ae946dcd0d01fb8c492af834f09446ca8886
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2021-10-10 22:04:35 +0100

    wordpress

--- srcs/requirements/nginx/tools/docker-nginx-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-nginx-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-openrc
-touch /run/openrc/softlevel
+#openrc
+#touch /run/openrc/softlevel
 mkdir -p /var/www/abbelhac
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=MO/ST=Khouribga/L=Khouribga/O=42/OU=1337Khouribga/CN=abbelhac.42.fr" -keyout /etc/ssl/private/abbelhac.42.fr.key -out /etc/ssl/certs/abbelhac.42.fr.crt
 cp /tmp/index.html /var/www/abbelhac
