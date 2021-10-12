d4e95af8216d0dcf86269e5eef4cf3554afb596d
Murasakibarachi <abdellahbelhachmi13915@gmail.com>
2021-10-12 15:37:55 +0100

    setup compose

--- srcs/requirements/nginx/tools/docker-nginx-entrypoint.sh
+++ srcs/requirements/nginx/tools/docker-nginx-entrypoint.sh
@@ -3,7 +3,10 @@
 #openrc
 #touch /run/openrc/softlevel
 mkdir -p /var/www/abbelhac
-openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=MO/ST=Khouribga/L=Khouribga/O=42/OU=1337Khouribga/CN=abbelhac.42.fr" -keyout /etc/ssl/private/abbelhac.42.fr.key -out /etc/ssl/certs/abbelhac.42.fr.crt
+if [! -e /etc/ssl/private/${DOMAINE_NAME}.key] || [! -e /etc/ssl/certs/${DOMAINE_NAME}.crt]; then
+	echo "create TLS certs & key file"
+	openssl req -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=MO/ST=Khouribga/L=Khouribga/O=42/OU=1337Khouribga/CN=abbelhac.42.fr" -keyout /etc/ssl/private/${DOMAINE_NAME}.key -out /etc/ssl/certs/${DOMAINE_NAME}.crt
+fi
 cp /tmp/index.html /var/www/abbelhac
 if [ -e /tmp/default.conf ]; then
 	mv /tmp/default.conf /etc/nginx/http.d/default.conf
@@ -11,4 +14,5 @@ fi
 #service nginx status
 #service nginx start
 #tail -f  /var/log/nginx/access.log
-exec "$@"
+nginx -g "daemon off;"
+#exec "$@"
