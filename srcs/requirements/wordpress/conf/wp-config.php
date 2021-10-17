0338231f5427fa2f014ab9310fb6e362c3ece8ba
murasakibarachi <abdellahbelhachmi13915@gmail.com>
2021-10-17 20:50:07 +0100

    setup maria

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+define( 'DB_NAME', ${MYSQL_DATABASE} );
 
 /** MySQL database username */
-define( 'DB_USER', 'username_here' );
+define( 'DB_USER', ${MYSQL_USER} );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'password_here' );
+define( 'DB_PASSWORD', ${MYSQL_PASSWORD} );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', ${DB_HOST} );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
+define('AUTH_KEY',         '66qqj!67XKQj^+{p}@lr?3.&NX!$K+v<8dFDl-f /+R1cXFlrrnmQW0-pm-{C*EL');
+define('SECURE_AUTH_KEY',  'kZ%/^d28U8_D1t948IVxOPQZ+Lrs8j cl$~zcm{,*f =WlusW%;^i2e-z[N.<gLd');
+define('LOGGED_IN_KEY',    'UdDBUqcVp$kb.oev?;+PN3%2*KG$&Ha4i8vi.7%h++uQ{7A,K.zo(;W2^>nu7eB:');
+define('NONCE_KEY',        '%-1de0/H-g1^x i!u&FX0ZlqS1pVmy*kH)Wo~QmlIWc}d$#g+/&$/M|xf~M.aG2*');
+define('AUTH_SALT',        '0uB0[X2BS4#dI{g->pgL|y4.jQLjXObXqOWyp#9aaI+~LJC||`,N)4K|&Bf4guZi');
+define('SECURE_AUTH_SALT', '36L#{Wle9/q+E*^h[5u*B-QR%^?|mkL/#)<[om@,*uN.b5~Fe:}U|t^uJ~-es}3+');
+define('LOGGED_IN_SALT',   'A}ubNdx;6P_$:KEfkZNIX1NN4=7-hm_jF=vgk_H<KP11)sMy7?RyxV2HurF3k`+}');
+define('NONCE_SALT',       'z_Z_;9P9opE}c6T&~%P&q`R|xYW18ju|N4Uy)OMlkHzQ~58YOi[V(dLK-|BKbg|2');
 
 /**#@-*/
 
--- srcs/requirements/wordpress/tools/docker-wordpress-entrypoint.sh
+++ srcs/requirements/wordpress/tools/docker-wordpress-entrypoint.sh
@@ -1,1 +1,9 @@
-#!/bin/ash
+#!/bin/sh
+
+if [ -e /tmp/wp-config.php ]; then
+	echo "setup wp-config file"
+	envsubst '$MYSQL_DATABASE $MYSQL_USER $MYSQL_PASSWORD $DB_HOST' < /tmp/wp-config.php > /var/www/wordpress/wp-config.php
+	rm -f /tmp/wp-config.php
+fi
+
+php-fpm7 -F
